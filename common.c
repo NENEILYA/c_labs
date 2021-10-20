c74e0a6f3a6b340f571650a7782d310b0c690fcf
ilyak <redfooqq@gmail.com>
2021-10-20 22:46:13 +0300

    - Обновлены общие функции

--- common.c
+++ common.c
@@ -1,13 +1,30 @@
 //
 // Created by Илья Коваленко on 17.10.2021.
 //
+#include <stdbool.h>
+#include <stdio.h>
 
-int check(void){
-    int a = 0;
-    while (scanf("%d",&a)!= 1 || a > 100 || a < 1){
+double check_double(void){
+    double a = 0;
+    while (scanf("%lf",&a)!= 1){
         while(getchar() != '\n'){
         }
         printf("Введите корректное число: ");
     }
     return a;
+}
+
+int check_int(void){
+    int a = 0;
+    while (scanf("%i",&a)!= 1){
+        while(getchar() != '\n'){
+        }
+        printf("Введите корректное число: ");
+    }
+    return a;
+}
+
+bool if_in_range(int x, int start, int end){
+    if (start <= x && x <= end) return true;
+    else return false;
 }
